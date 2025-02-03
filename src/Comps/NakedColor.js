ff34d7142a3b7cee16fd4e0a209794bfafa0debc
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2025-02-03 15:33:11 -0500

    Took out the fragment that was wrappping the text, as it was already inside a div

--- src/Comps/NakedColor.js
+++ src/Comps/NakedColor.js
@@ -3,7 +3,7 @@
 const NakedColor = ({ colors, curly1, curly2, appliedColor }) => {
     return (
         <div style={{ color: appliedColor }}>
-          <>{curly1} id: {colors.id} , title: {colors.title} {curly2}</>
+          {curly1} id: {colors.id} , title: {colors.title} {curly2}
         </div>
     );
 
